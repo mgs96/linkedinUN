88f6a2bc7cf1dc642797327673bd9669651c77af
Mauricio <mauricio.guzman.salazar@gmail.com>
2016-10-23 22:47:50 -0500

    attempt to upload neo4j to heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+
+  config.neo4j.session_type = :server_db
+  config.neo4j.session_path = ENV["GRAPHENEDB_URL"] || 'http://localhost:7474'
+  
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
